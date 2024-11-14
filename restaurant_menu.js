45a83cb86064a7b4d2ad1fcc43110cfcdd4d1f96
Oladipupo Adenowun <oladipupo.adenowun@yahoo.com>
2024-11-14 17:34:50 -0500

    added the count of items

--- restaurant_menu.js
+++ restaurant_menu.js
@@ -4,6 +4,7 @@ const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index)=>`<p>Item ${index+1}: ${item}</p>`).join('');
 document.getElementById("breakfastMenuItems").innerHTML = breakfastMenuItemsHTML;
+document.getElementById("breakfastTotalItems").innerHTML = `${breakfastMenu.length} items`;
 
 /* iterate with forEach which does not return output*/
 let mainCourseMenuItemsHTML = '';
@@ -11,6 +12,7 @@ mainCourseMenu.forEach((item,index)=>{
     mainCourseMenuItemsHTML += `<p>Item ${index+1}: ${item}</p>`;
 });
 document.getElementById("maincourseMenuItems").innerHTML = mainCourseMenuItemsHTML;
+document.getElementById("maincourseTotalItems").innerHTML = `${mainCourseMenu.length} items`;
 
 
 /* iterate with for loop */
@@ -19,3 +21,4 @@ for(let index=0; index<dessertMenu.length; index++){
     desertMenuItemsHTML += `<p>Item ${index+1}: ${dessertMenu[index]}</p>`;
 }
 document.getElementById("dessertMenuItems").innerHTML = desertMenuItemsHTML;
+document.getElementById("dessertTotalItems").innerHTML = `${dessertMenu.length} items`;
